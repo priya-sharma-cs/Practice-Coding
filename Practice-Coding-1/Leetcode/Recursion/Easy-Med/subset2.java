94fb1b622e762822dc94a0ce80d05a5267ed9145
priya sharma <priya.sharma.200530@gmail.com>
2026-08-31 22:08:24 +0530

    subset2

---
+++ Practice-Coding-1/Leetcode/Recursion/Easy-Med/subset2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        backtrack(0, nums, new ArrayList<>(), ans);
+
+        return ans;
+    }
+
+    static void backtrack(int index, int[] nums,
+                          List<Integer> current,
+                          List<List<Integer>> ans) {
+
+        // Current subset ko answer mein add karo
+        ans.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+
+            // Duplicate ko skip karo
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Include
+            current.add(nums[i]);
+
+            // Recursion
+            backtrack(i + 1, nums, current, ans);
+
+            // Backtrack
+            current.remove(current.size() - 1);
+        }
+    }
+}
